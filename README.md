fc5b4232b5c7560d36b26d9107d4334a886882db
Julie Nielsen <jpettesen98@gmail.com>
2022-11-18 13:31:04 +0100

    first commit

--- README.md
+++ README.md
@@ -1,13 +1,118 @@
+# Mandatory Assignment - Web Technologies
+
+## Description
+
+Database for the government to:
+
+- Access data for a given store type
+- Access data for a given area
+- Access data for a single user/costumer
+- Access ALL data for a given date OR month
+- Erase all data for a given user
+
+## Installation
+
+Install dependancies:
+
+```
+npm install
+```
+
+Use start script:
+
+```
+npm start
+```
+
 ## Usage
 
-POST example:
-{
-"name": "Vendela",
-"age": "1",
-"owner_name": "Tore",
-"species": "Bird"
+```node
+Express app, storing sessions in memory with SQLITE3 database.
+
+# Use Postman to GET:
+Location: localhost:8081/location/:store_location
+Store: localhost:8081/store/:store_name
+Costumer/User: localhost:8081/card/:card_number
+Data for given date: localhost:8081/day/:date
+Data for given Month: localhost:8081/month/:month/:year
+All stored cards: localhost:8081/cards
+All stored items: localhost:8081/items
+
+# Use Postman to POST:
+New items: localhost:8081/item
+Example:
+ {
+    "name": "Vanilla Icecream",
+    "category": "Dairy",
+    "price": "19,00",
+    "order_number": 1101 //!important to not use a ordernumber thats already been used, use /items to check the last used ordernumber
 }
 
-# bugs:
+! Use following categories when posting new item:
+    // Beverages – coffee/tea, juice, soda
+    // Bread/Bakery – sandwich loaves, dinner rolls, tortillas, bagels
+    // Canned/Jarred Goods – vegetables, spaghetti sauce, ketchup
+    // Dairy – cheeses, eggs, milk, yogurt, butter
+    // Dry/Baking Goods – cereals, flour, sugar, pasta, mixes
+    // Frozen Foods – waffles, vegetables, individual meals, ice cream
+    // Meat – lunch meat, poultry, beef, pork
+    // Produce – fruits, vegetables
+    // Cleaners – all- purpose, laundry detergent, dishwashing liquid/detergent
+    // Paper Goods – paper towels, toilet paper, aluminium foil, sandwich bags
+    // Personal Care – shampoo, soap, hand soap, shaving cream
+    // Confectionery – Sweets, Chocolates, Ice-cream, Cakes
+    // Other – baby items, pet items, batteries, greeting cards
 
-path to select pet by ID doesn´t work.
+New costumer/user: localhost:8081/card
+Example:
+{
+    "card_number": 1897,
+    "store_name": "Kiwi",
+    "store_location": "Stathelle, Bamble",
+    "date": 5,
+    "month_number": 11,
+    "year_number": 22
+    "recipt_number": 1101 //!SAME number as order_number items table.
+}
+
+# Use Postman to DELETE:
+Deleting user/costumer:localhost:8081/card/:card_number
+```
+
+## Dependencies
+
+    "body-parser": "^1.20.1"
+     - body-parser extracts the entire body portion of an incoming request stream and exposes it on req. body
+
+    "cookie-parser": "^1.4.6"
+     - cookie-parser is a middleware which parses cookies attached to the client request object.
+
+    "express": "^4.18.2"
+     - express is a node js web application framework to build web and mobile applications. It's a layer built on the top of the Node js that helps manage servers and routes.
+
+    "express-router": "^0.0.1"
+     - creates a router as a module, loads a middleware function in it, defines some routes, and mounts the router module on a path in the main app.
+
+    "express-session": "^1.17.3"
+     - express-session is an HTTP server-side framework used to create and manage a session middleware.
+
+    "morgan": "^1.10.0"
+     - Morgan is an HTTP request level Middleware for Node.js, great to use for debugging because it logs the requests.
+
+    "path": "^0.12.7"
+     - NodeJS path module is a core built-in module. It provides functionalities for accessing and interacting with files.
+
+    "sqlite": "^4.1.2"
+     - SQLite consist of a single file and a library to make your application interact with it. Used to to manage data and data definition in RDBMS.
+     Example: "SELECT * FROM users WHERE name = 'Julie';"
+
+    "sqlite3": "^5.1.2"
+     - is just the version 3 of SQLite which provides - more compact format for database files. Supports both UTF-8 and UTF-16 text.
+
+## Author
+
+Julie Nielsen (@julienielsen98)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const sqlite3 = require("sqlite3");
 const morgan = require("morgan");
 
 const express = require("express");
+const router = express.Router();
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 const app = express();
 
 const db = new sqlite3.Database(__dirname + "/database.sqlite");
@@ -14,131 +15,335 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+// CREATEING TABLES
 
-const CREATE_PETS =
-  "CREATE TABLE if not exists pets (pet_ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTERGER, owner_name TEXT, species TEXT);";
-const DROP_PETS = "DROP TABLE if exists pets;";
+//CREATE/DROP setup for ITEMS
+const CREATE_ITEMS =
+  "CREATE TABLE if not exists items (item_ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, category TEXT, price INTERGER, order_number INTERGER);";
+const DROP_ITEMS = "DROP TABLE if exists items;";
 
+//CREATE/DROP setup for CARDS
+const CREATE_CARD =
+  "CREATE TABLE if not exists cards (card_ID INTEGER PRIMARY KEY AUTOINCREMENT, card_number INTERGER, store_name TEXT, store_location TEXT, date INTERGER, month_number INTERGER, year_number INTERGER, recipt_number INTERGER);";
+const DROP_CARD = "DROP TABLE if exists cards;";
+
+// GET STATEMENTS
+
+//Creating the tables
 app.get("/create", (req, res) => {
-  db.run(CREATE_PETS);
-  res.send("Table created");
+  db.run(CREATE_ITEMS);
+  db.run(CREATE_CARD);
+  res.status(201).send("Table created");
 });
 
+//Dropping the tables
 app.get("/drop", (req, res) => {
-  db.run(DROP_PETS);
-  res.send("Table dropped");
+  db.run(DROP_ITEMS);
+  db.run(DROP_CARD);
+  res.status(204).send("Table dropped");
 });
 
+//Resets the tables
 app.get("/reset", (req, res) => {
-  db.run(DROP_PETS, () => {
+  db.run(DROP_ITEMS, () => {
     console.log("Table dropped ...");
-    db.run(CREATE_PETS, () => {
+    db.run(CREATE_ITEMS, () => {
       console.log("...  and re-created");
 
       db.run(
-        "INSERT INTO pets (name, age, owner_name, species) VALUES ('Bo',  '2',  'Julie',  'Dog');"
-      );
-      db.run(
-        "INSERT INTO pets (name, age, owner_name, species) VALUES ('Nicci',  '3',  'Hilde',  'Cat');"
+        `INSERT INTO items (name, category, price, order_number) VALUES (?,  ?,  null, ?);`
       );
     });
   });
 
-  res.send("Table reset (dropped and re-created)");
+  db.run(DROP_CARD, () => {
+    console.log("Table dropped ...");
+    db.run(CREATE_CARD, () => {
+      console.log("...  and re-created");
+
+      db.run(
+        "INSERT INTO cards (card_number, store_name, store_location, date, month_number, year_number, recipt_number) VALUES (null,  ?,  ?, ?, ?, ?, ?);"
+      );
+    });
+  });
+
+  res.sendStatus(205);
 });
 
-app.get("/show", (req, res) => {
+// MAIN ENTRY. Returns frontpage with usage description on /
+router.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+//Returns data stored in tables
+app.get("/showall", (req, res) => {
   let data = [];
+
+  let order_number = req.params.order_number;
+  let recipt_number = req.params.recipt_number;
+
   db.serialize(() => {
     db.each(
-      "SELECT * FROM pets;",
+      "SELECT * FROM items, cards WHERE order_number AND recipt_number IS NOT NULL;",
+      order_number,
+      recipt_number,
       (err, row) => {
-        console.log(row.name);
+        console.log(row);
         data.push(row);
       },
       () => {
-        res.send(data);
+        res.status(200).send(data);
       }
     );
   });
 });
 
-app.post("/pet", (req, res) => {
+//Returns all stored ITEMS
+app.get("/items", (req, res) => {
+  let items = [];
+
+  db.serialize(() => {
+    db.each(
+      "SELECT * FROM items WHERE price IS NOT NULL;",
+      (err, row) => {
+        console.log(row.name);
+        items.push(row);
+      },
+      () => {
+        res.status(200).send({ items });
+      }
+    );
+  });
+});
+
+//Returns all stored CARDS
+app.get("/cards", (req, res) => {
+  let cards = [];
+  db.serialize(() => {
+    db.each(
+      "SELECT * FROM cards WHERE recipt_number IS NOT NULL;",
+      (err, row) => {
+        console.log(row.card_number);
+        cards.push(row);
+      },
+      () => {
+        res.status(200).send({ cards });
+      }
+    );
+  });
+});
+
+//Returns all data stored to a single CARD
+app.get("/card/:card_number", (req, res) => {
+  let data = [];
+  let card_number = req.params.card_number;
+  let order_number = req.params.order_number;
+  let recipt_number = req.params.recipt_number;
+
+  db.serialize(() => {
+    db.each(
+      "SELECT * FROM cards, items WHERE recipt_number == order_number AND card_number == ?",
+      recipt_number | order_number | card_number,
+      (err, row) => {
+        console.log(row);
+        data.push(row);
+      },
+      () => {
+        res.status(200).send(data);
+      }
+    );
+  });
+});
+
+//Returns data stored to a single DATE
+app.get("/day/:date", (req, res) => {
+  let data = [];
+
+  let date = req.params.date;
+  let recipt_number = req.params.recipt_number;
+  let order_number = req.params.order_number;
+
+  db.serialize(() => {
+    db.each(
+      "SELECT name, price, category, card_number, date, month_number, year_number FROM cards, items WHERE order_number IS NOT NULL AND recipt_number == order_number AND date == ?",
+      recipt_number | order_number | date,
+      (err, row) => {
+        console.log(row);
+        data.push(row);
+      },
+      () => {
+        res.status(200).send(data);
+      }
+    );
+  });
+});
+
+//Returns data stored to a MONTH/YEAR combination
+app.get("/month/:month_number/:year_number", (req, res) => {
+  let data = [];
+
+  let month_number = req.params.month_number;
+  let year_number = req.params.year_number;
+  let order_number = req.params.order_number;
+  let recipt_number = req.params.recipt_number;
+
+  db.serialize(() => {
+    db.each(
+      "SELECT name, price, category, card_number, date, month_number, year_number FROM cards, items WHERE order_number IS NOT NULL AND recipt_number == order_number AND month_number | year_number == ?",
+      order_number | recipt_number | month_number | year_number,
+      (err, row) => {
+        console.log(row);
+        data.push(row);
+      },
+      () => {
+        res.status(200).send(data);
+      }
+    );
+  });
+});
+
+//Returns all data stored to a LOCATION ( on /City, State (!must include comma and space between, and capital letters))
+app.get("/location/:store_location", (req, res) => {
+  let data = [];
+
+  let order_number = req.params.order_number;
+  let recipt_number = req.params.recipt_number;
+  let store_location = req.params.store_location;
+
+  db.serialize(() => {
+    db.each(
+      "SELECT * FROM cards, items WHERE store_location == ? AND order_number IS NOT NULL AND recipt_number == order_number",
+      store_location,
+      order_number,
+      recipt_number,
+      (err, row) => {
+        console.log(row.store_location);
+        data.push(row);
+      },
+      () => {
+        res.status(200).send(data);
+      }
+    );
+  });
+});
+
+//Returns all data stored to a single STORE (on /Store (!must include capital letter))
+app.get("/store/:store_name", (req, res) => {
+  let data = [];
+
+  let order_number = req.params.order_number;
+  let recipt_number = req.params.recipt_number;
+  let store_name = req.params.store_name;
+
+  db.serialize(() => {
+    db.each(
+      "SELECT * FROM cards, items WHERE store_name == ? AND order_number IS NOT NULL AND recipt_number == order_number",
+      store_name,
+      order_number,
+      recipt_number,
+      (err, row) => {
+        console.log(row.store_name);
+        data.push(row);
+      },
+      () => {
+        res.status(200).send(data);
+      }
+    );
+  });
+});
+
+// POST STATEMENTS
+
+//POSTING ITEM, usage: Postman
+app.post("/item", (req, res) => {
   console.log(req.body);
 
   let name = req.body.name;
-  let age = req.body.age;
-  let owner_name = req.body.owner_name;
-  let species = req.body.species;
+  let category = req.body.category;
+  let price = req.body.price;
+  let order_number = req.body.order_number;
 
   db.run(
-    "INSERT INTO pets (name, age, owner_name, species) VALUES ('" +
+    "INSERT INTO items (name, category, price, order_number) VALUES ('" +
       name +
       "',  '" +
-      age +
+      category + // ! Use following categories:
+      // Beverages – coffee/tea, juice, soda
+      // Bread/Bakery – sandwich loaves, dinner rolls, tortillas, bagels
+      // Canned/Jarred Goods – vegetables, spaghetti sauce, ketchup
+      // Dairy – cheeses, eggs, milk, yogurt, butter
+      // Dry/Baking Goods – cereals, flour, sugar, pasta, mixes
+      // Frozen Foods – waffles, vegetables, individual meals, ice cream
+      // Meat – lunch meat, poultry, beef, pork
+      // Produce – fruits, vegetables
+      // Cleaners – all- purpose, laundry detergent, dishwashing liquid/detergent
+      // Paper Goods – paper towels, toilet paper, aluminium foil, sandwich bags
+      // Personal Care – shampoo, soap, hand soap, shaving cream
+      // Confectionery – Sweets, Chocolates, Ice-cream, Cakes
+      // Other – baby items, pet items, batteries, greeting cards
       "', '" +
-      owner_name +
+      price +
       "', '" +
-      species +
+      order_number + //DO NOT not use an ordernumber thats already been used, use /items to check the last used ordernumber
       "');"
   );
-  res.send("Saved");
+  res.status(201).send("Saved");
 });
 
-app.put("/pet", (req, res) => {
+//POSTING CARD, usage: Postman
+app.post("/card", (req, res) => {
+  console.log(req.body);
+
+  let card_number = req.body.card_number;
+  let store_name = req.body.store_name;
+  let store_location = req.body.store_location;
+  let date = req.body.date;
+  let month_number = req.body.month_number;
+  let year_number = req.body.year_number;
+  let recipt_number = req.body.recipt_number;
+
+  db.run(
+    "INSERT INTO cards (card_number, store_name, store_location, date, month_number, year_number, recipt_number) VALUES ('" +
+      card_number +
+      "',  '" +
+      store_name +
+      "', '" +
+      store_location +
+      "', '" +
+      date +
+      "', '" +
+      month_number +
+      "', '" +
+      year_number +
+      "', '" +
+      recipt_number + //!SAME number as order_number at items table.
+      "');"
+  );
+
+  res.status(201).send("Saved");
+});
+
+// DELETE STATEMENT
+
+//DELETING CHOSEN CARD on /:card_number
+app.delete("/card/:card_number", (req, res) => {
   let data = [];
+  let card_number = req.params.card_number;
+
   db.serialize(() => {
     db.each(
-      "SELECT * FROM pets;",
+      "DELETE FROM cards WHERE card_number == ?",
+      card_number,
       (err, row) => {
-        console.log(row.name);
+        console.log(`${row.card_number}, ${row.name}`);
         data.push(row);
       },
       () => {
-        res.send(data);
+        res.status(204).send("Deleted");
       }
     );
   });
 });
 
-app.get("/pet/:id", (req, res) => {
-  let data = [];
-  let pet_id = req.params.id;
-
-  db.serialize(() => {
-    db.each(
-      "SELECT * FROM pets WHERE pet_ID == ?",
-      pet_id,
-      (err, row) => {
-        console.log(row.name);
-        data.push(row);
-      },
-      () => {
-        res.send(data);
-      }
-    );
-  });
-});
-app.delete("/pet/:id", (req, res) => {
-  let data = [];
-  let pet_id = req.params.id;
-
-  db.serialize(() => {
-    db.each(
-      "DELETE FROM pets WHERE pet_ID == ?",
-      pet_id,
-      (err, row) => {
-        console.log(row.name);
-        data.push(row);
-      },
-      () => {
-        res.send(data);
-      }
-    );
-  });
-});
-
+app.use("/", router);
 app.listen(port, () => console.log("Server is running on port:", port));
